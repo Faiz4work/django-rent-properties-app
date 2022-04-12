f04eb2c6e09eb90b5a5022687bc1e681cc6969da
faiz4work <faizahmed11234@gmail.com>
2022-04-12 01:41:25 -0700

    added renewal date table with a range of 60 days and redirect to property details page when click on a property.

--- renewal/views.py
+++ renewal/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from properties.models import Property
+import datetime
 
 # Create your views here.
 def renewal_page(request):
-    return render(request, "renewal/renewal_page.html")
+    renwal_days_limit = 60
+    now = datetime.datetime.now()
+    two_months_later = now + datetime.timedelta(days=renwal_days_limit)
+    props = Property.objects.filter(renewal_date__range=(now,two_months_later))
+    context = {
+        "props": props,
+    }
+    return render(request, "renewal/renewal_page.html", context)
+
+
+
+
